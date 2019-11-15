626bed541061968050de708fc01a44c4b8a99672
unknown <496733391@qq.com>
2019-11-15 22:31:38 +0800

    update

--- src/sk_learn/titanic.py
+++ src/sk_learn/titanic.py
@@ -32,8 +32,8 @@ def divide_df(all_data):
     return all_data.loc[:890], all_data.loc[891:].drop(['Survived'], axis=1)
 
 
-df_train = pd.read_csv('C:\Users\lenovo\Desktop\machine-learning-explore\data/train.csv')
-df_test = pd.read_csv('C:\Users\lenovo\Desktop\machine-learning-explore\data/test.csv')
+df_train = pd.read_csv(BASE_DIR + '\data/train.csv')
+df_test = pd.read_csv(BASE_DIR + '\data//test.csv')
 df_all = df_train.append(df_test, ignore_index=True)
 
 df_train.name = 'Training Set'
@@ -406,3 +406,305 @@ plt.legend(['Not Survived', 'Survived'], loc='upper right', prop={'size': 15})
 plt.title('Count of Survival in {} Feature'.format('Ticket Frequency'), size=15, y=1.05)
 
 plt.show()
+
+df_all['Title'] = df_all['Name'].str.split(', ', expand=True)[1].str.split('.', expand=True)[0]
+df_all['Is_Married'] = 0
+df_all['Is_Married'].loc[df_all['Title'] == 'Mrs'] = 1
+
+fig, axs = plt.subplots(nrows=2, figsize=(20, 20))
+sns.barplot(x=df_all['Title'].value_counts().index, y=df_all['Title'].value_counts().values, ax=axs[0])
+
+axs[0].tick_params(axis='x', labelsize=10)
+axs[1].tick_params(axis='x', labelsize=15)
+
+for i in range(2):
+    axs[i].tick_params(axis='y', labelsize=15)
+
+axs[0].set_title('Title Feature Value Counts', size=20, y=1.05)
+
+df_all['Title'] = df_all['Title'].replace(['Miss', 'Mrs','Ms', 'Mlle', 'Lady', 'Mme', 'the Countess', 'Dona'], 'Miss/Mrs/Ms')
+df_all['Title'] = df_all['Title'].replace(['Dr', 'Col', 'Major', 'Jonkheer', 'Capt', 'Sir', 'Don', 'Rev'], 'Dr/Military/Noble/Clergy')
+
+sns.barplot(x=df_all['Title'].value_counts().index, y=df_all['Title'].value_counts().values, ax=axs[1])
+axs[1].set_title('Title Feature Value Counts After Grouping', size=20, y=1.05)
+
+plt.show()
+
+
+def extract_surname(data):
+    families = []
+
+    for i in range(len(data)):
+        name = data.iloc[i]
+
+        if '(' in name:
+            name_no_bracket = name.split('(')[0]
+        else:
+            name_no_bracket = name
+
+        family = name_no_bracket.split(',')[0]
+        title = name_no_bracket.split(',')[1].strip().split(' ')[0]
+
+        for c in string.punctuation:
+            family = family.replace(c, '').strip()
+
+        families.append(family)
+
+    return families
+
+
+df_all['Family'] = extract_surname(df_all['Name'])
+df_train = df_all.loc[:890]
+df_test = df_all.loc[891:]
+dfs = [df_train, df_test]
+
+# Creating a list of families and tickets that are occuring in both training and test set
+non_unique_families = [x for x in df_train['Family'].unique() if x in df_test['Family'].unique()]
+non_unique_tickets = [x for x in df_train['Ticket'].unique() if x in df_test['Ticket'].unique()]
+
+df_family_survival_rate = df_train.groupby('Family')['Survived', 'Family','Family_Size'].median()
+df_ticket_survival_rate = df_train.groupby('Ticket')['Survived', 'Ticket','Ticket_Frequency'].median()
+
+family_rates = {}
+ticket_rates = {}
+
+for i in range(len(df_family_survival_rate)):
+    # Checking a family exists in both training and test set, and has members more than 1
+    if df_family_survival_rate.index[i] in non_unique_families and df_family_survival_rate.iloc[i, 1] > 1:
+        family_rates[df_family_survival_rate.index[i]] = df_family_survival_rate.iloc[i, 0]
+
+for i in range(len(df_ticket_survival_rate)):
+    # Checking a ticket exists in both training and test set, and has members more than 1
+    if df_ticket_survival_rate.index[i] in non_unique_tickets and df_ticket_survival_rate.iloc[i, 1] > 1:
+        ticket_rates[df_ticket_survival_rate.index[i]] = df_ticket_survival_rate.iloc[i, 0]
+
+mean_survival_rate = np.mean(df_train['Survived'])
+
+train_family_survival_rate = []
+train_family_survival_rate_NA = []
+test_family_survival_rate = []
+test_family_survival_rate_NA = []
+
+for i in range(len(df_train)):
+    if df_train['Family'][i] in family_rates:
+        train_family_survival_rate.append(family_rates[df_train['Family'][i]])
+        train_family_survival_rate_NA.append(1)
+    else:
+        train_family_survival_rate.append(mean_survival_rate)
+        train_family_survival_rate_NA.append(0)
+
+for i in range(len(df_test)):
+    if df_test['Family'].iloc[i] in family_rates:
+        test_family_survival_rate.append(family_rates[df_test['Family'].iloc[i]])
+        test_family_survival_rate_NA.append(1)
+    else:
+        test_family_survival_rate.append(mean_survival_rate)
+        test_family_survival_rate_NA.append(0)
+
+df_train['Family_Survival_Rate'] = train_family_survival_rate
+df_train['Family_Survival_Rate_NA'] = train_family_survival_rate_NA
+df_test['Family_Survival_Rate'] = test_family_survival_rate
+df_test['Family_Survival_Rate_NA'] = test_family_survival_rate_NA
+
+train_ticket_survival_rate = []
+train_ticket_survival_rate_NA = []
+test_ticket_survival_rate = []
+test_ticket_survival_rate_NA = []
+
+for i in range(len(df_train)):
+    if df_train['Ticket'][i] in ticket_rates:
+        train_ticket_survival_rate.append(ticket_rates[df_train['Ticket'][i]])
+        train_ticket_survival_rate_NA.append(1)
+    else:
+        train_ticket_survival_rate.append(mean_survival_rate)
+        train_ticket_survival_rate_NA.append(0)
+
+for i in range(len(df_test)):
+    if df_test['Ticket'].iloc[i] in ticket_rates:
+        test_ticket_survival_rate.append(ticket_rates[df_test['Ticket'].iloc[i]])
+        test_ticket_survival_rate_NA.append(1)
+    else:
+        test_ticket_survival_rate.append(mean_survival_rate)
+        test_ticket_survival_rate_NA.append(0)
+
+df_train['Ticket_Survival_Rate'] = train_ticket_survival_rate
+df_train['Ticket_Survival_Rate_NA'] = train_ticket_survival_rate_NA
+df_test['Ticket_Survival_Rate'] = test_ticket_survival_rate
+df_test['Ticket_Survival_Rate_NA'] = test_ticket_survival_rate_NA
+
+for df in [df_train, df_test]:
+    df['Survival_Rate'] = (df['Ticket_Survival_Rate'] + df['Family_Survival_Rate']) / 2
+    df['Survival_Rate_NA'] = (df['Ticket_Survival_Rate_NA'] + df['Family_Survival_Rate_NA']) / 2
+
+non_numeric_features = ['Embarked', 'Sex', 'Deck', 'Title', 'Family_Size_Grouped', 'Age', 'Fare']
+
+for df in dfs:
+    for feature in non_numeric_features:
+        df[feature] = LabelEncoder().fit_transform(df[feature])
+
+cat_features = ['Pclass', 'Sex', 'Deck', 'Embarked', 'Title', 'Family_Size_Grouped']
+encoded_features = []
+
+for df in dfs:
+    for feature in cat_features:
+        encoded_feat = OneHotEncoder().fit_transform(df[feature].values.reshape(-1, 1)).toarray()
+        n = df[feature].nunique()
+        cols = ['{}_{}'.format(feature, n) for n in range(1, n + 1)]
+        encoded_df = pd.DataFrame(encoded_feat, columns=cols)
+        encoded_df.index = df.index
+        encoded_features.append(encoded_df)
+
+df_train = pd.concat([df_train, *encoded_features[:6]], axis=1)
+df_test = pd.concat([df_test, *encoded_features[6:]], axis=1)
+
+df_all = df_train.append(df_test, ignore_index=True)
+drop_cols = ['Deck', 'Embarked', 'Family', 'Family_Size', 'Family_Size_Grouped', 'Survived',
+             'Name', 'Parch', 'PassengerId', 'Pclass', 'Sex', 'SibSp', 'Ticket', 'Title',
+            'Ticket_Survival_Rate', 'Family_Survival_Rate', 'Ticket_Survival_Rate_NA', 'Family_Survival_Rate_NA']
+
+df_all.drop(columns=drop_cols, inplace=True)
+
+print(df_all.head())
+
+X_train = StandardScaler().fit_transform(df_train.drop(columns=drop_cols))
+y_train = df_train['Survived'].values
+X_test = StandardScaler().fit_transform(df_test.drop(columns=drop_cols))
+
+print('X_train shape: {}'.format(X_train.shape))
+print('y_train shape: {}'.format(y_train.shape))
+print('X_test shape: {}'.format(X_test.shape))
+
+single_best_model = RandomForestClassifier(criterion='gini',
+                                           n_estimators=1100,
+                                           max_depth=5,
+                                           min_samples_split=4,
+                                           min_samples_leaf=5,
+                                           max_features='auto',
+                                           oob_score=True,
+                                           random_state=SEED,
+                                           n_jobs=-1,
+                                           verbose=1)
+
+leaderboard_model = RandomForestClassifier(criterion='gini',
+                                           n_estimators=1750,
+                                           max_depth=7,
+                                           min_samples_split=6,
+                                           min_samples_leaf=6,
+                                           max_features='auto',
+                                           oob_score=True,
+                                           random_state=SEED,
+                                           n_jobs=-1,
+                                           verbose=1)
+
+N = 5
+oob = 0
+probs = pd.DataFrame(np.zeros((len(X_test), N * 2)),
+                     columns=['Fold_{}_Prob_{}'.format(i, j) for i in range(1, N + 1) for j in range(2)])
+importances = pd.DataFrame(np.zeros((X_train.shape[1], N)), columns=['Fold_{}'.format(i) for i in range(1, N + 1)],
+                           index=df_all.columns)
+fprs, tprs, scores = [], [], []
+
+skf = StratifiedKFold(n_splits=N, random_state=N, shuffle=True)
+
+for fold, (trn_idx, val_idx) in enumerate(skf.split(X_train, y_train), 1):
+    print('Fold {}\n'.format(fold))
+
+    # Fitting the model
+    leaderboard_model.fit(X_train[trn_idx], y_train[trn_idx])
+
+    # Computing Train AUC score
+    trn_fpr, trn_tpr, trn_thresholds = roc_curve(y_train[trn_idx],
+                                                 leaderboard_model.predict_proba(X_train[trn_idx])[:, 1])
+    trn_auc_score = auc(trn_fpr, trn_tpr)
+    # Computing Validation AUC score
+    val_fpr, val_tpr, val_thresholds = roc_curve(y_train[val_idx],
+                                                 leaderboard_model.predict_proba(X_train[val_idx])[:, 1])
+    val_auc_score = auc(val_fpr, val_tpr)
+
+    scores.append((trn_auc_score, val_auc_score))
+    fprs.append(val_fpr)
+    tprs.append(val_tpr)
+
+    # X_test probabilities
+    probs.loc[:, 'Fold_{}_Prob_0'.format(fold)] = leaderboard_model.predict_proba(X_test)[:, 0]
+    probs.loc[:, 'Fold_{}_Prob_1'.format(fold)] = leaderboard_model.predict_proba(X_test)[:, 1]
+    importances.iloc[:, fold - 1] = leaderboard_model.feature_importances_
+
+    oob += leaderboard_model.oob_score_ / N
+    print('Fold {} OOB Score: {}\n'.format(fold, leaderboard_model.oob_score_))
+
+print('Average OOB Score: {}'.format(oob))
+
+importances['Mean_Importance'] = importances.mean(axis=1)
+importances.sort_values(by='Mean_Importance', inplace=True, ascending=False)
+
+plt.figure(figsize=(15, 20))
+sns.barplot(x='Mean_Importance', y=importances.index, data=importances)
+
+plt.xlabel('')
+plt.tick_params(axis='x', labelsize=15)
+plt.tick_params(axis='y', labelsize=15)
+plt.title('Random Forest Classifier Mean Feature Importance Between Folds', size=15)
+
+plt.show()
+
+
+def plot_roc_curve(fprs, tprs):
+    tprs_interp = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+    f, ax = plt.subplots(figsize=(15, 15))
+
+    # Plotting ROC for each fold and computing AUC scores
+    for i, (fpr, tpr) in enumerate(zip(fprs, tprs), 1):
+        tprs_interp.append(np.interp(mean_fpr, fpr, tpr))
+        tprs_interp[-1][0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        ax.plot(fpr, tpr, lw=1, alpha=0.3, label='ROC Fold {} (AUC = {:.3f})'.format(i, roc_auc))
+
+    # Plotting ROC for random guessing
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', alpha=0.8, label='Random Guessing')
+
+    mean_tpr = np.mean(tprs_interp, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+
+    # Plotting the mean ROC
+    ax.plot(mean_fpr, mean_tpr, color='b', label='Mean ROC (AUC = {:.3f} $\pm$ {:.3f})'.format(mean_auc, std_auc), lw=2,
+            alpha=0.8)
+
+    # Plotting the standard deviation around the mean ROC Curve
+    std_tpr = np.std(tprs_interp, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label='$\pm$ 1 std. dev.')
+
+    ax.set_xlabel('False Positive Rate', size=15, labelpad=20)
+    ax.set_ylabel('True Positive Rate', size=15, labelpad=20)
+    ax.tick_params(axis='x', labelsize=15)
+    ax.tick_params(axis='y', labelsize=15)
+    ax.set_xlim([-0.05, 1.05])
+    ax.set_ylim([-0.05, 1.05])
+
+    ax.set_title('ROC Curves of Folds', size=20, y=1.02)
+    ax.legend(loc='lower right', prop={'size': 13})
+
+    plt.show()
+
+
+plot_roc_curve(fprs, tprs)
+
+class_survived = [col for col in probs.columns if col.endswith('Prob_1')]
+probs['1'] = probs[class_survived].sum(axis=1) / N
+probs['0'] = probs.drop(columns=class_survived).sum(axis=1) / N
+probs['pred'] = 0
+pos = probs[probs['1'] >= 0.5].index
+probs.loc[pos, 'pred'] = 1
+
+y_pred = probs['pred'].astype(int)
+
+submission_df = pd.DataFrame(columns=['PassengerId', 'Survived'])
+submission_df['PassengerId'] = df_test['PassengerId']
+submission_df['Survived'] = y_pred.values
