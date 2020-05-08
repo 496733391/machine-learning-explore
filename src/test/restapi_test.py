0b1fe943b3cc1f2cc1b55461ad03df7c4914ee42
unknown <496733391@qq.com>
2020-05-08 16:43:22 +0800

    update

--- src/test/restapi_test.py
+++ src/test/restapi_test.py
@@ -9,9 +9,59 @@ app = Flask(__name__)
 api = Api(app)
 
 
-@app.route('/helloworld/')
-def hello_world():
-    return jsonify({'hello': {'world': {'1': '3'}}})
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
+# TODOS = {x: x*2 for x in range(100000)}
+
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('task', type=str)
+
+
+#  show a single todo item and lets you delete them
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+
+# Actually setup the Api resource routing here
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
 
 
 if __name__ == '__main__':
